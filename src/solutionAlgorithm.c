df572c68b1dfcaf15576928533180eb91c0e891d
OUHIDA Ayoub <private.ouhida@gmail.com>
2025-08-02 18:21:47 +0100

    Editing the logic little bit and fill the "Output" string
    - Adding the "if" condition statement to check if the number of rows equal to "1".
    - If yes, return the string as it is, nothing gonna change.
    - Filling the Zigzag map with value "0". To select only the values with sense later, when we start filling the "Output" string.
    - Allocate memory for a string "finalString" with the same size of the original one + "1" to hold the NULL Terminator '\0' and return the first character of the string later.
    - Initialize an index for this string "finalStringIndex" with value "0".
    - Nested loop to fill the "finalString" with NON-"0" values cells.
    - Terminate the "finalString"  with NULL Terminator '\0' and return the first character of the string.

--- src/solutionAlgorithm.c
+++ src/solutionAlgorithm.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-char* convert(char* s, int numRows) {
+char *convert(char* s, int numRows) {
+
+    if (numRows == 1) return s;
 
     size_t stringLength = strlen(s);
     int stringTracker = 0;
 
     char zigzagMap [numRows][stringLength];
+    memset(zigzagMap, 0, sizeof zigzagMap);
 
     int a = 0;
     int b = 0;
@@ -18,6 +21,9 @@ char* convert(char* s, int numRows) {
 
     while(stringTracker != stringLength){
 
+        zigzagMap [a][b] = s[stringTracker];
+        stringTracker++;
+
         if ( (a == 0) ){
             growVertical = 1;
             growDiagonal = 0;
@@ -26,18 +32,31 @@ char* convert(char* s, int numRows) {
             growDiagonal = 1;
         }
 
-        zigzagMap [a][b] = s[stringTracker];
-        stringTracker++;
-
-
-
         if ( growVertical == 1) {
             a++;
         } else if ( growDiagonal = 1){
             a--;
             b++;
         }
-
+        
     }
+
+    char *finalString = malloc(stringLength + 1);
+    int finalStringIndex = 0;
+
+    for (int i = 0; i < numRows; i++){
+        for(int j = 0; j < stringLength; j++){
+            if ( zigzagMap [i][j] != '\0' ){
+                finalString[finalStringIndex] = zigzagMap [i][j];
+                finalStringIndex++;
+            }else{
+                continue;
+            }
+        }
+    }
+
+    finalString[finalStringIndex] = '\0';
+
+    return finalString;
 
 }
